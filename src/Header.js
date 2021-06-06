e501574fed4d7165d8b54c2ee4cb976fc303c49c
Ernesto Carrillo <ernesto.carrillo10@icloud.com>
2021-06-05 23:59:48 -0400

    finished navbar

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,14 @@
 import React from 'react'
+
 import './Header.css';
 import PetsIcon from '@material-ui/icons/Pets';
-
+import HeaderOption from './HeaderOption.js';
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+ 
 function Header() {
     return (
         <div className='header'>
@@ -16,7 +23,13 @@ function Header() {
             </div>
 
             <div className='header__right'>
-            
+                <HeaderOption Icon={HomeIcon} title="Home" />
+                <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+                <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+                <HeaderOption Icon={ChatIcon} title="Messaging" />
+                <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+                <HeaderOption avatar="https://pics.me.me/thumb_cute-doge-meme-doge-dog-doggo-pupper-dogecoin-art-50112985.png" title="Me" />
+
             </div>
             
         </div>
